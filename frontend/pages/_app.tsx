626f6745541c531312144373e7d54286f0b3d539
sbateeni <121227261+sbateeni@users.noreply.github.com>
2025-07-15 14:27:46 +0300

    .

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,5 @@
-import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 } 
